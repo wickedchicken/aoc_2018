f4b88729fa251344a31d332ea1ba2a3fb7b664c4
Mike Stipicevic <stipredirect@gmail.com>
2018-12-21 02:02:24 +0100

    Finish day 1 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,11 +1,43 @@
 use crate::file_reader::read_input;
 use std::collections::HashMap;
 
-pub fn run() -> i32 {
+fn compare_str(a: &str, b: &str) -> String {
+    let a_chars = a.chars();
+    let mut b_chars = b.chars();
+
+    assert_eq!(a.len(), b.len());
+
+    let mut matches = "".to_string();
+    for a_char in a_chars {
+        let b_char = b_chars.next().unwrap();
+        if a_char == b_char {
+            matches.push(a_char);
+        }
+    }
+
+    matches
+}
+
+pub fn run() -> (i32, String) {
     let mut twos = 0;
     let mut threes = 0;
 
+    let mut box_ids: Vec<String> = Vec::new();
+    let mut output: Option<String> = None;
+
     for line in read_input("input/input2.txt".to_string()) {
+        if output.is_none() {
+            let trimmed = line.trim().to_string();
+            for id in box_ids.iter() {
+                let common = compare_str(&trimmed, id);
+                if common.len() == &trimmed.len() - 1 {
+                    output = Some(common.clone());
+                    break;
+                }
+            }
+            box_ids.push(trimmed);
+        }
+
         let mut seen_characters = HashMap::new();
         for c in line.chars() {
             let stat = seen_characters.entry(c).or_insert(0);
@@ -24,5 +56,5 @@ pub fn run() -> i32 {
         }
     }
 
-    twos * threes
+    (twos * threes, output.unwrap())
 }
